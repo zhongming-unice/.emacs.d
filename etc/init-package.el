4c3b30bf70484154bbe698bea500a2cd99b21b73
ming <ming.zhong@etu.unice.fr>
2021-03-07 10:19:45 +0800

    remove ccls package

--- etc/init-package.el
+++ etc/init-package.el
@@ -84,31 +84,31 @@
   :ensure t 
   :config (turn-on-page-break-lines-mode))
 
-(use-package vterm
-    :ensure t)
+;; (use-package vterm
+;;     :ensure t)
 
-(use-package ccls
-  :ensure t
-  :defer t
-  :hook ((c-mode c++-mode objc-mode) . (lambda () (require 'ccls) (lsp)))
-  :custom
-  (ccls-executable (executable-find "ccls"))
-  (ccls-sem-highlight-method 'font-lock)
-  (ccls-enable-skipped-ranges nil)
-  ;; :config
-  ;; (lsp-register-client
-  ;;  (make-lsp--client	
-  ;; 	:new-connection (lsp-tramp-connection (cons ccls-executable ccls-args))
-  ;; 	:major-modes '(c-mode c++-mode)
-  ;; 	:server-id 'ccls-remote
-  ;; 	:multi-root nil
-  ;; 	:remote? t
-  ;; 	;; :notification-handlers
-  ;; 	;; (lsp-ht ("$ccls/publishSkippedRanges" #'ccls--publish-skipped-ranges)
-  ;; 	;; 		("$ccls/publishSemanticHighlight" #'ccls--publish-semantic-highlight))
-  ;; 	:initialization-options (lambda () ccls-initialization-options)
-  ;; 	:library-folders-fn nil))
-  )
+;; (use-package ccls
+;;   :ensure t
+;;   :defer t
+;;   :hook ((c-mode c++-mode objc-mode) . (lambda () (require 'ccls) (lsp)))
+;;   :custom
+;;   (ccls-executable (executable-find "ccls"))
+;;   (ccls-sem-highlight-method 'font-lock)
+;;   (ccls-enable-skipped-ranges nil)
+;;   ;; :config
+;;   ;; (lsp-register-client
+;;   ;;  (make-lsp--client	
+;;   ;; 	:new-connection (lsp-tramp-connection (cons ccls-executable ccls-args))
+;;   ;; 	:major-modes '(c-mode c++-mode)
+;;   ;; 	:server-id 'ccls-remote
+;;   ;; 	:multi-root nil
+;;   ;; 	:remote? t
+;;   ;; 	;; :notification-handlers
+;;   ;; 	;; (lsp-ht ("$ccls/publishSkippedRanges" #'ccls--publish-skipped-ranges)
+;;   ;; 	;; 		("$ccls/publishSemanticHighlight" #'ccls--publish-semantic-highlight))
+;;   ;; 	:initialization-options (lambda () ccls-initialization-options)
+;;   ;; 	:library-folders-fn nil))
+;;   )
 
 (use-package modern-cpp-font-lock
   :ensure t
